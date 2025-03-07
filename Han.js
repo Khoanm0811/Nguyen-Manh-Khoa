7860baaa8addaf6b1108d8bd49c6a1bb0f505656
Khoanm0811 <khoaanguyenn0811@gmail.com>
2025-03-07 09:31:20 +0700

    Create Han.js

---
+++ Han.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = pass.value
+  if (u=="khoanm0811" && p=="8386")
+  {
+    window.open("Contents/home.html")
+    documet.getElementById(compliler).style.display="block"
+  }
+}
